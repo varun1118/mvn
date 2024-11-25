b93059ef58cd6c541656056812dec2cfb33ffc70
Varun . <varun2018sharmavk@gmail.com>
2024-11-25 20:24:28 +0530

    meaven

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/org/example/StudentMain.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Student stu = new Student();
+    }
+}
